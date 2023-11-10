c68ac69c49c8b8fe87dfc17001336ee997ab4682
M Mudassir Shakeel <112872954+MMudassirShakeel@users.noreply.github.com>
2023-11-10 09:05:44 +0500

    banchan.m

--- banchan.m
+++ banchan.m
@@ -1,6 +1,15 @@
-function [w,p]=banchan(w,p,xOc,yOc,zOc)
-     [w,p]=vedt(w,p,xOc-5,yOc-3,zOc,xOc+5,yOc-3,zOc);
-     [w,p]=vedt(w,p,xOc-5,yOc+3,zOc,xOc+5,yOc+3,zOc);
-     [w,p]=vedt(w,p,xOc-5,yOc-3,zOc,xOc-5,yOc+3,zOc);
-     [w,p]=vedt(w,p,xOc+5,yOc-3,zOc,xOc+5,yOc+3,zOc);
+function [w, p] = banchan(w, p, xOc, yOc, zOc)
+    % Function to draw a rectangular prism representing the body of the robot
+
+    % Draw the front side of the body
+    [w, p] = vedt(w, p, xOc - 5, yOc - 3, zOc, xOc + 5, yOc - 3, zOc);
+
+    % Draw the back side of the body
+    [w, p] = vedt(w, p, xOc - 5, yOc + 3, zOc, xOc + 5, yOc + 3, zOc);
+
+    % Draw the left side of the body
+    [w, p] = vedt(w, p, xOc - 5, yOc - 3, zOc, xOc - 5, yOc + 3, zOc);
+
+    % Draw the right side of the body
+    [w, p] = vedt(w, p, xOc + 5, yOc - 3, zOc, xOc + 5, yOc + 3, zOc);
 end
