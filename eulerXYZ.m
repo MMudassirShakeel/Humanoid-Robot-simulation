d6e68759a731117ddc521301773d19b3bffa1ef3
M Mudassir Shakeel <112872954+MMudassirShakeel@users.noreply.github.com>
2023-11-10 09:06:12 +0500

    eulerXYZ.m

--- eulerXYZ.m
+++ eulerXYZ.m
@@ -1,21 +1,27 @@
 function R = eulerXYZ(a1, a2, a3)
-% Convert XYZ Euler angles to rotation matrix
+    % Function to convert XYZ Euler angles to a rotation matrix
 
-R1 = [
-1, 0, 0;
-0, cos(a1), -sin(a1);
-0, sin(a1), cos(a1)];
+    % Rotation matrix around the X-axis
+    R1 = [
+        1, 0, 0;
+        0, cos(a1), -sin(a1);
+        0, sin(a1), cos(a1)
+    ];
 
-R2 = [
-cos(a2), 0, sin(a2);
-0, 1, 0;
--sin(a2), 0, cos(a2)];
+    % Rotation matrix around the Y-axis
+    R2 = [
+        cos(a2), 0, sin(a2);
+        0, 1, 0;
+        -sin(a2), 0, cos(a2)
+    ];
 
-R3 = [
-cos(a3), -sin(a3), 0;
-sin(a3), cos(a3), 0;
-0, 0, 1];
+    % Rotation matrix around the Z-axis
+    R3 = [
+        cos(a3), -sin(a3), 0;
+        sin(a3), cos(a3), 0;
+        0, 0, 1
+    ];
 
-R = R1*R2*R3;
-
+    % Combine the individual rotation matrices to get the overall rotation matrix
+    R = R1 * R2 * R3;
 end
